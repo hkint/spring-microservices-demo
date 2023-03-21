6ef532273b84752234563561d6700f35f55d08c3
HK <admin@hktwilight.eu.org>
2023-03-21 22:41:25 +0800

    [Add] department-service.yml

--- department-service/src/main/java/org/example/department/DepartmentServiceApplication.java
+++ department-service/src/main/java/org/example/department/DepartmentServiceApplication.java
@@ -1,4 +1,4 @@
-package org.example.departmentservice;
+package org.example.department;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
